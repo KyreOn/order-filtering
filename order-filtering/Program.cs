3cedc0619640507927aa4332e0d51b30d7807b02
KyreOn <kireonbk@gmail.com>
2024-10-24 18:20:19 +0400

    Querying and output

--- order-filtering/Program.cs
+++ order-filtering/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Globalization;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace order_filtering;
@@ -7,8 +9,8 @@ class Program
 {
     private static string _cityDistrict;
     private static DateTime _firstDeliveryDateTime = DateTime.Now;
-    private static string _deliveryLog;
-    private static string _deliveryOrder;
+    private static FileStream _deliveryLog;
+    private static FileStream _deliveryOrder;
     
     static void Main(string[] args)
     {
@@ -25,23 +27,24 @@ class Program
         }
         else
             SetFieldsFromArgs(args);
+        var queryResult = Query();
+        PrintResult(queryResult);
+        _deliveryOrder.Close();
+        _deliveryLog.Close();
     }
 
     #region DataInput
 
     static void SetFieldsFromArgs(string[] args)
     {
-        if (!ValidateDistrict(args[0]) || !ValidateDate(args[1]) ||
-            !ValidatePath(args[2]) || !ValidatePath(args[3]))
+        if (!ValidateDistrict(args[0], out _cityDistrict) || !ValidateDate(args[1], out _firstDeliveryDateTime) ||
+            !ValidatePath(args[2], out _deliveryLog) || !ValidatePath(args[3], out _deliveryOrder))
         {
             Console.WriteLine("Неверные параметры. Проверьте правильность введенных данных, либо запустите программу без параметров");
             Environment.Exit(1);
         }
             
         _cityDistrict = args[0];
-        _firstDeliveryDateTime = DateTime.ParseExact(args[1], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-        _deliveryLog = args[2];
-        _deliveryOrder = args[3];
     }
 
     static void InputDistrict()
@@ -49,15 +52,9 @@ class Program
         while (true)
         {
             var district = Console.ReadLine();
-            if (ValidateDistrict(district))
-                _cityDistrict = district;
-            else
-            {
-                Console.WriteLine("Район доставки введен неверно, проверьте правильность введеных данных и повторите попытку:");
-                continue;
-            }
-
-            break;
+            if (ValidateDistrict(district, out _cityDistrict))
+                break;
+            Console.WriteLine("Район доставки введен неверно, проверьте правильность введеных данных и повторите попытку:");
         }
     }
 
@@ -66,15 +63,9 @@ class Program
         while (true)
         {
             var date = Console.ReadLine();
-            if (ValidateDate(date))
-                _firstDeliveryDateTime = DateTime.ParseExact(date, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-            else
-            {
-                Console.WriteLine("Дата доставки введена неверно, проверьте правильность введеных данных и повторите попытку:");
-                continue;
-            }
-
-            break;
+            if (ValidateDate(date, out _firstDeliveryDateTime))
+                break;
+            Console.WriteLine("Дата доставки введена неверно, проверьте правильность введеных данных и повторите попытку:");
         }
     }
 
@@ -83,15 +74,9 @@ class Program
         while (true)
         {
             var logPath = Console.ReadLine();
-            if (ValidatePath(logPath))
-                _deliveryLog = logPath;
-            else
-            {
-                Console.WriteLine("Путь к файлу введен неверно, проверьте правильность введеных данных и повторите попытку:");
-                continue;
-            }
-
-            break;
+            if (ValidatePath(logPath, out _deliveryLog))
+                break;
+            Console.WriteLine("Путь к файлу введен неверно, проверьте правильность введеных данных и повторите попытку:");
         }
     }
 
@@ -100,15 +85,9 @@ class Program
         while (true)
         {
             var orderPath = Console.ReadLine();
-            if (ValidatePath(orderPath))
-                _deliveryOrder = orderPath;
-            else
-            {
-                Console.WriteLine("Путь к файлу введен неверно, проверьте правильность введеных данных и повторите попытку:");
-                continue;
-            }
-
-            break;
+            if (ValidatePath(orderPath, out _deliveryOrder))
+                break;
+            Console.WriteLine("Путь к файлу введен неверно, проверьте правильность введеных данных и повторите попытку:");
         }
     }
 
@@ -116,12 +95,57 @@ class Program
     
     #region Validation
 
-    static bool ValidateDistrict(string district) => !string.IsNullOrEmpty(district);
+    static bool ValidateDistrict(string district, out string districtName)
+    {
+        districtName = district;
+        return !string.IsNullOrEmpty(district);
+    }
 
-    static bool ValidateDate(string date) => 
-        DateTime.TryParseExact(date, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+    static bool ValidateDate(string date, out DateTime dateTime) => 
+        DateTime.TryParseExact(date, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime);
 
-    static bool ValidatePath(string path) => Path.Exists(path);
+    static bool ValidatePath(string path, out FileStream file)
+    {
+        try
+        {
+            file = File.Create(path);
+            return true;
+        }
+        catch (Exception)
+        {
+            file = null;
+            return false;
+        }
+    }
+    
+    #endregion
 
+    #region Query
+    
+    static IEnumerable<JsonNode> Query()
+    {
+        var json = JsonNode.Parse(File.ReadAllText("test-data.json"));
+        var result = json.AsArray()
+            .Where(x => CheckEntry(x["district"].ToString(), x["deliveryDateTime"].ToString()))
+            .Select(x => x);
+        return result;
+    }
+
+    static bool CheckEntry(string district, string data)
+    {
+        var diff = DateTime.ParseExact(data, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture) - _firstDeliveryDateTime;
+        return diff.TotalSeconds is > 0 and <= 18000 && district == _cityDistrict;
+    }
+
+    static void PrintResult(IEnumerable<JsonNode> result)
+    {
+        foreach (var entry in result)
+        {
+            var text = $"Номер заказа: {entry["id"]}, Вес заказа(кг): {entry["weight"]}, Район заказа: {entry["district"]}, Дата доставки: {entry["deliveryDateTime"]}\n";
+            var bytes = Encoding.UTF8.GetBytes(text);
+            _deliveryOrder.Write(bytes);
+        }
+    }
+    
     #endregion
 }
